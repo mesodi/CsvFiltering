b159e30a5c8a987853b1f39abb498e8702c660b8
stykle <Maxamadabdulahist@outlook.com>
2024-02-09 09:28:36 +0100

    Initial commit

---
+++ src/main/java/es/wacoco/csvfiltering/Main.java
@@ -0,0 +1,13 @@
+package es.wacoco.csvfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
