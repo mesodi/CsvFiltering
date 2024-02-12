5f80228f3fd17d81232214a608ebc3031e4ce859
RobKaya <robkaya@icloud.com>
2024-02-12 13:28:16 +0100

    createJobID function added

--- src/main/java/es/wacoco/csvfiltering/service/JobService.java
+++ src/main/java/es/wacoco/csvfiltering/service/JobService.java
@@ -3,6 +3,7 @@ package es.wacoco.csvfiltering.service;
 import es.wacoco.csvfiltering.model.Job;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class JobService {
     ArrayList<Job> database = new ArrayList<>();
@@ -14,4 +15,29 @@ public class JobService {
 
         return null;
     }
+
+    public String createJobID(String date, JobService jobService) {
+        Random random = new Random();
+
+        Boolean idTaken;
+        String newID;
+
+        do {
+            StringBuilder idBuilder = new StringBuilder("QRY-");
+            int uniqueNum = 100000 + random.nextInt(900000);
+            idBuilder.append(date.replace("-", "") + "-" + Integer.toString(uniqueNum));
+            newID = idBuilder.toString();
+            idTaken = false;
+            for (Job job : jobService.database) {
+                if (job.getJobID().equals(idBuilder)) {
+                    idTaken = true;
+                    break;
+                }
+            }
+
+
+        } while (idTaken);
+        return newID;
+    }
+}
 }
