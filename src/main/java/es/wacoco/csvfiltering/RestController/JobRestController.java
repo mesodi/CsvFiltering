d8fcb4d19d378bfb1b4ff922af25566e8ef3f194
stykle <Maxamadabdulahist@outlook.com>
2024-02-13 09:25:51 +0100

    Getting all the jobs from backend and displaying on frontend everytime page loads

--- src/main/java/es/wacoco/csvfiltering/RestController/JobRestController.java
+++ src/main/java/es/wacoco/csvfiltering/RestController/JobRestController.java
@@ -1,6 +1,9 @@
 package es.wacoco.csvfiltering.RestController;
 
+import es.wacoco.csvfiltering.model.Job;
+import es.wacoco.csvfiltering.service.JobService;
 import org.apache.camel.ProducerTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +12,13 @@ import java.util.*;
 
 @org.springframework.web.bind.annotation.RestController
 public class JobRestController {
+    private final JobService jobService;
+
 
     private final ProducerTemplate template;
 
-    public JobRestController(ProducerTemplate template) {
+    public JobRestController(JobService jobService, ProducerTemplate template) {
+        this.jobService = jobService;
         this.template = template;
     }
 
@@ -22,6 +28,11 @@ public class JobRestController {
            @RequestParam("file") MultipartFile file,@RequestParam("fields") List<String> fields) {
 
         return template.requestBodyAndHeader("direct:processCsv", file, "userFields", fields);
+    }
+
+    @GetMapping("/all")
+    public List<Job> getAllJobs() {
+        return jobService.getAllJobs();
     }
 }
 
